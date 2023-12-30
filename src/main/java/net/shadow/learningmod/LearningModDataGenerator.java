f1bff751f9c6ee89dd9a5270ebf7a8516688ff95
ShadowIsHereAndIsVeryCute <moonlord.iseasy@gmail.com>
2023-12-30 13:26:06 +0100

    Updated

--- src/main/java/net/shadow/learningmod/LearningModDataGenerator.java
+++ src/main/java/net/shadow/learningmod/LearningModDataGenerator.java
@@ -2,10 +2,16 @@ package net.shadow.learningmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.shadow.learningmod.datagen.*;
 
 public class LearningModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
